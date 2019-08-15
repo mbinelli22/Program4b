d63d6dbb839cb34758f92640e6accdd99d055184
mbinelli22 <mbinelli1996@gmail.com>
2019-08-15 18:22:19 -0500

    initial commit

---
+++ src/Program4b/PigLatin.java
@@ -0,0 +1,13 @@
+package Program4b;
+//Michael Binelli
+import java.util.Scanner;
+public class PigLatin {
+	public static void main(String[] args) {
+		Scanner stdIn = new Scanner(System.in);
+		
+		
+		
+		stdIn.close();
+	}
+	
+}
