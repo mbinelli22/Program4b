2aada0af649e55023ceb4eb96449796ff63e72d5
mbinelli22 <mbinelli1996@gmail.com>
2019-08-15 18:32:10 -0500

    added do while loop to know whether or not to play game

--- src/Program4b/PigLatin.java
+++ src/Program4b/PigLatin.java
@@ -4,7 +4,11 @@ import java.util.Scanner;
 public class PigLatin {
 	public static void main(String[] args) {
 		Scanner stdIn = new Scanner(System.in);
+		String word;
+		String pigLatinWord;
 		
+		System.out.println("Welcome to Pig Latin Translator");
+		System.out.println("-------------------------------");
 		
 		
 		stdIn.close();
